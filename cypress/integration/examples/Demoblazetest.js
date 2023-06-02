d8725b3398dab40d32ca5d6ad035124c1e03a061
Yan K <yanqapl@gmail.com>
2023-06-02 04:22:24 +0300

    path updating

--- cypress/integration/examples/Demoblazetest.js
+++ cypress/integration/examples/Demoblazetest.js
@@ -2,54 +2,58 @@
 
 describe('Home page Test Suite', () => {
     beforeEach(() => {
-        cy.visit('https://www.demoblaze.com/');
-      });
-
+        cy.visit('https://www.demoblaze.com/')
+    })
+    
     //Check that pictures in the slider can be scrolled
     it('Pictures in the slider can be scrolled', () => {
-        cy.get('.carousel-control-next-icon').click()
         cy.wait(1000)
-        cy.get('.carousel-control-next-icon').click()
+        cy.get('img[src="Samsung1.jpg"]').should('be.visible')
+        cy.get('a.carousel-control-next').click()                                                            
         cy.wait(1000)
-        cy.get('.carousel-control-prev-icon').click()
+        cy.get('img[src="nexus1.jpg"]').should('be.visible')
+        cy.get('a.carousel-control-next').click()
+        cy.wait(1000)
+        cy.get('img[src="iphone1.jpg"]').should('be.visible')
+        cy.get('a.carousel-control-prev').click()
+        cy.get('img[src="nexus1.jpg"]').should('be.visible')
     })
-
     //Check that categories change the displayed product on the page
     it('Categories change the displayed product on the page', () => {
+        cy.get('a.list-group-item').contains("Phones").click()
         cy.wait(1000)
-        cy.contains('Phones').click()
-        cy.get('.card-title').should('be.visible').and('have.length', 7) 
-        cy.contains('Laptops').click()
-        cy.get('.card-title').should('be.visible').and('have.length', 6)
+        cy.get('h4.card-title').should('be.visible').and('have.length', 7) 
+        cy.get('a.list-group-item').contains("Laptops").click()
+        cy.get('h4.card-title').should('be.visible').and('have.length', 6)
         cy.wait(1000)
-        cy.contains('Monitor').click()
-        cy.get('.card-title').should('be.visible').and('have.length', 2)
+        cy.get('a.list-group-item').contains("Monitor").click()
+        cy.get('h4.card-title').should('be.visible').and('have.length', 2)
     })
 
     //Check that the products are displayed with their detailed information (name, price and description)
     it('Products are displayed with their detailed information', () => {
-        cy.contains('Phones').click()
-        cy.get('.card-block').each((card) => {
+        cy.get('a.list-group-item').contains("Phones").click()
+        cy.get('div.card-block').each((card) => {
             cy.wrap(card).within(() => {
-                cy.get('.card-title').should('be.visible')
+                cy.get('h4.card-title').should('be.visible')
                 cy.get('h5').should('be.visible').contains('$')
-                cy.get('#article').should('be.visible')
+                cy.get('p#article.card-text').should('be.visible')
               })
         })
-        cy.contains('Laptops').click()
-        cy.get('.card-block').each((card) => {
+        cy.get('a.list-group-item').contains("Laptops").click()
+        cy.get('div.card-block').each((card) => {
             cy.wrap(card).within(() => {
-                cy.get('.card-title').should('be.visible')
+                cy.get('h4.card-title').should('be.visible')
                 cy.get('h5').should('be.visible').contains('$')
-                cy.get('#article').should('be.visible')
+                cy.get('p#article.card-text').should('be.visible')
               });
         });
-        cy.contains('Monitor').click()
-        cy.get('.card-block').each((card) => {
+        cy.get('a.list-group-item').contains("Monitor").click()
+        cy.get('div.card-block').each((card) => {
             cy.wrap(card).within(() => {
-                cy.get('.card-title').should('be.visible')
-                cy.get('h5').should('be.visible').contains('$') 
-                cy.get('#article').should('be.visible')
+                cy.get('h4.card-title').should('be.visible')
+                cy.get('h5').should('be.visible').contains('$')
+                cy.get('p#article.card-text').should('be.visible')
               })
         })
     })
@@ -57,46 +61,57 @@ describe('Home page Test Suite', () => {
     //Check that when you click on a product, you are redirected to the card of the corresponding product
     it('Click on a product redirecting to the card of the corresponding product', () => {
         cy.wait(1000)
-        cy.contains('Monitor').click()
+        cy.get('a.list-group-item').contains("Monitor").click()
         cy.wait(1000)
-        cy.get('.card').each(($card) => {
-          cy.contains('Monitor').click()
-          const productTitle = $card.find('.card-title').text()
+        cy.get('div.card').each(($card) => {
+          cy.get('a.list-group-item').contains("Monitor").click()
+          const productTitle = $card.find('h4.card-title').text()
           cy.contains(productTitle).click()
-          cy.get('.name').should('have.text', productTitle)
+          cy.get('h2.name').should('have.text', productTitle)
           cy.go('back')
         })
     })
-    
+})
+
+
+describe('Product page Test Suite', () => {
+    beforeEach(() => {
+        cy.visit('https://www.demoblaze.com/')
+    })
 
     //Check that when you go to the product card, the name, price, description, product picture and the “Add to cart” button are displayed
     it('When you go to the product card, the name, price, description, product picture and the “Add to cart” button are displayed', () => {
-      cy.get('.card').contains('Samsung galaxy s6').click()
-      cy.get('.name').should('have.text',`Samsung galaxy s6`)
-      cy.get('.price-container').should('be.visible').contains('$')
-      cy.get(".description").should('be.visible')
-      cy.get(".item > img").should('be.visible')
-      cy.contains('Add to cart').should('be.visible')
+      cy.get('div.card').contains("Samsung galaxy s6").click()
+      cy.get('h2.name').should('have.text',`Samsung galaxy s6`)
+      cy.get('h3.price-container').should('be.visible').contains('$')
+      cy.get('div#myTabContent.tab-content').should('be.visible')
+      cy.get('div.item > img').should('be.visible')
+      cy.get('a.btn-success').contains('Add to cart').should('be.visible')
     })
+
 
     //Check that when clicking on the “Add to cart” button, the product is added to the cart
     it('When clicking on the “Add to cart” button, the product is added to the cart', () => {
-      cy.get('.card').contains('Samsung galaxy s6').click()
-      cy.contains('Add to cart').click()
+      cy.get('div.card').contains('Samsung galaxy s6').click()
+      cy.get('a.btn-success').contains('Add to cart').click()
       cy.wait(1000)
       cy.on('window:alert',(str)=>
         {
             expect(str).to.equal('Product added')
         })
       cy.wait(2000)
-      cy.contains('Cart').click()
-      cy.get('.success').contains('Samsung galaxy s6')
+      cy.get('a#cartur.nav-link').contains('Cart').click()
+      cy.get('tr.success').contains('Samsung galaxy s6')
     })
-    
-
+})
+/*
+describe('Cart page Test Suite', () => {
+    beforeEach(() => {
+        cy.visit('https://www.demoblaze.com/')
+    })
     //Check that the shopping cart page opens and closes
     it('Shopping cart page opens and closes', () => {
-      cy.contains('Cart').click()
+      cy.get(.)contains('Cart').click()               //сделать проверку что url изменился
       cy.contains('Home').click()
     })
 
@@ -128,6 +143,8 @@ describe('Home page Test Suite', () => {
       cy.contains('Place Order').click()
       cy.get('.modal-dialog').should('be.visible')
     })
-})
 
+    
+})
+*/
 
