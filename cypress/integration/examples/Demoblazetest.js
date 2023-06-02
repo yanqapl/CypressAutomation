499f872acb2377ccd807a493a39dd0d64a7be201
Yan K <yanqapl@gmail.com>
2023-06-02 17:27:37 +0300

    Last fixes

--- cypress/integration/examples/Demoblazetest.js
+++ cypress/integration/examples/Demoblazetest.js
@@ -4,35 +4,39 @@ describe('Home page Test Suite', () => {
     beforeEach(() => {
         cy.visit('https://www.demoblaze.com/')
     })
-    
-    //Check that pictures in the slider can be scrolled
+
+    const categoryPhonesLocator = 'a[onclick="byCat(\'phone\')"]'
+    const categoryLaptopsLocator = 'a[onclick="byCat(\'notebook\')"]'
+    const categoryMonitorsLocator = 'a[onclick="byCat(\'monitor\')"]'
+ 
     it('Pictures in the slider can be scrolled', () => {
-        cy.wait(1000)
-        cy.get('img[src="Samsung1.jpg"]').should('be.visible')
-        cy.get('a.carousel-control-next').click()                                                            
-        cy.wait(1000)
-        cy.get('img[src="nexus1.jpg"]').should('be.visible')
-        cy.get('a.carousel-control-next').click()
-        cy.wait(1000)
-        cy.get('img[src="iphone1.jpg"]').should('be.visible')
-        cy.get('a.carousel-control-prev').click()
-        cy.get('img[src="nexus1.jpg"]').should('be.visible')
-    })
-    //Check that categories change the displayed product on the page
-    it('Categories change the displayed product on the page', () => {
-        cy.get('a.list-group-item').contains("Phones").click()
-        cy.wait(1000)
-        cy.get('h4.card-title').should('be.visible').and('have.length', 7) 
-        cy.get('a.list-group-item').contains("Laptops").click()
-        cy.get('h4.card-title').should('be.visible').and('have.length', 6)
-        cy.wait(1000)
-        cy.get('a.list-group-item').contains("Monitor").click()
-        cy.get('h4.card-title').should('be.visible').and('have.length', 2)
+        const nextBottonCarousel = 'a.carousel-control-next'
+        const prevBottonCarousel = 'a.carousel-control-prev'
+
+        cy.get('img[alt="First slide"]').should('be.visible')
+        cy.get(nextBottonCarousel).click()
+        cy.wait(1000).get('img[alt="Second slide"]').should('be.visible')
+        cy.get(nextBottonCarousel).click()
+        cy.wait(1000).get('img[alt="Third slide"]').should('be.visible')
+        cy.get(prevBottonCarousel).click()
+        cy.wait(1000).get('img[alt="Second slide"]').should('be.visible')
     })
 
-    //Check that the products are displayed with their detailed information (name, price and description)
+  
+    it('Categories change the displayed product on the page', () => {
+        const productPageLocator = 'div[class="card h-100"]'
+
+        //TO DO: I understand that comparing by the number of products isn't the best idea but in theory as an option to get these values, we can query them from the database
+        cy.wait(1000).get(categoryPhonesLocator).click()
+        cy.get(productPageLocator).should('be.visible').and('have.length', 7)
+        cy.wait(1000).get(categoryLaptopsLocator).click()
+        cy.get(productPageLocator).should('be.visible').and('have.length', 6)
+        cy.get(categoryMonitorsLocator).click()
+        cy.get(productPageLocator).should('be.visible').and('have.length', 2)
+    })
+
     it('Products are displayed with their detailed information', () => {
-        cy.get('a.list-group-item').contains("Phones").click()
+        cy.get(categoryPhonesLocator).click()
         cy.get('div.card-block').each((card) => {
             cy.wrap(card).within(() => {
                 cy.get('h4.card-title').should('be.visible')
@@ -40,7 +44,7 @@ describe('Home page Test Suite', () => {
                 cy.get('p#article.card-text').should('be.visible')
               })
         })
-        cy.get('a.list-group-item').contains("Laptops").click()
+        cy.get(categoryLaptopsLocator).click()
         cy.get('div.card-block').each((card) => {
             cy.wrap(card).within(() => {
                 cy.get('h4.card-title').should('be.visible')
@@ -48,22 +52,19 @@ describe('Home page Test Suite', () => {
                 cy.get('p#article.card-text').should('be.visible')
               });
         });
-        cy.get('a.list-group-item').contains("Monitor").click()
+        cy.get(categoryMonitorsLocator).click()
         cy.get('div.card-block').each((card) => {
             cy.wrap(card).within(() => {
                 cy.get('h4.card-title').should('be.visible')
-                cy.get('h5').should('be.visible').contains('$')
+                cy.get('h5').should('be.visible').contains('$') //TO DO: Need to find out options, how to check the price regardless of the value
                 cy.get('p#article.card-text').should('be.visible')
               })
         })
     })
-    
-    //Check that when you click on a product, you are redirected to the card of the corresponding product
+
     it('Click on a product redirecting to the card of the corresponding product', () => {
-        cy.wait(1000)
-        cy.get('a.list-group-item').contains("Monitor").click()
-        cy.wait(1000)
-        cy.get('div.card').each(($card) => {
+        cy.wait(1000).get(categoryMonitorsLocator).click()
+        cy.wait(1000).get('div.card').each(($card) => {
           cy.get('a.list-group-item').contains("Monitor").click()
           const productTitle = $card.find('h4.card-title').text()
           cy.contains(productTitle).click()
@@ -73,14 +74,12 @@ describe('Home page Test Suite', () => {
     })
 })
 
-
 describe('Product page Test Suite', () => {
     beforeEach(() => {
         cy.visit('https://www.demoblaze.com/')
     })
 
-    //Check that when you go to the product card, the name, price, description, product picture and the “Add to cart” button are displayed
-    it('When you go to the product card, the name, price, description, product picture and the “Add to cart” button are displayed', () => {
+    it('Check that main elements of product card are displayed', () => {
       cy.get('div.card').contains("Samsung galaxy s6").click()
       cy.get('h2.name').should('have.text',`Samsung galaxy s6`)
       cy.get('h3.price-container').should('be.visible').contains('$')
@@ -89,62 +88,58 @@ describe('Product page Test Suite', () => {
       cy.get('a.btn-success').contains('Add to cart').should('be.visible')
     })
 
-
-    //Check that when clicking on the “Add to cart” button, the product is added to the cart
     it('When clicking on the “Add to cart” button, the product is added to the cart', () => {
       cy.get('div.card').contains('Samsung galaxy s6').click()
-      cy.get('a.btn-success').contains('Add to cart').click()
-      cy.wait(1000)
+      cy.wait(1000).get('a.btn-success').contains('Add to cart').click()
       cy.on('window:alert',(str)=>
         {
             expect(str).to.equal('Product added')
         })
-      cy.wait(2000)
-      cy.get('a#cartur.nav-link').contains('Cart').click()
+      cy.wait(2000).get('a#cartur.nav-link').contains('Cart').click()
       cy.get('tr.success').contains('Samsung galaxy s6')
     })
 })
-/*
+
 describe('Cart page Test Suite', () => {
     beforeEach(() => {
         cy.visit('https://www.demoblaze.com/')
     })
-    //Check that the shopping cart page opens and closes
+
     it('Shopping cart page opens and closes', () => {
-      cy.get(.)contains('Cart').click()               //сделать проверку что url изменился
-      cy.contains('Home').click()
+      cy.wait(1000).url().should('not.include',"/cart").get('a#cartur.nav-link').contains('Cart').click()
+      cy.url().should('include',"/cart")
+      cy.go('back').url().should('not.include',"/cart")
     })
 
-    //Check that multiple products can be added to the cart
+  
     it('Multiple products can be added to the cart', () => {
-      cy.get('.card').contains('Samsung galaxy s6').click()    
-      cy.contains('Add to cart').click().click()
-      cy.contains('Cart').click()
+      cy.get('div.card').contains('Samsung galaxy s6').click()    
+      cy.get('a.btn-success').contains('Add to cart').click().type("{enter}")
+      cy.wait(1000).get('a.btn-success').contains('Add to cart').click().type("{enter}")
+      cy.get('a#cartur.nav-link').contains('Cart').click()
+      cy.get('tr.success').should('have.length',2).contains('Samsung galaxy s6')
     })
-
-    //Check that items in the cart can be removed
+  
     it('Items in the cart can be removed', () => {
-      cy.get('.card').contains('Samsung galaxy s6').click()
-      cy.contains('Add to cart').click().click()
-      cy.contains('Cart').click()
-      cy.wait(2000)
-      cy.get('.success').each((product) => {
-        cy.wrap(product).within(() => {
+      cy.get('div.card').contains('Samsung galaxy s6').click()
+      cy.get('a.btn-success').contains('Add to cart').click().type("{enter}")
+      cy.wait(1000).get('a.btn-success').contains('Add to cart').click().type("{enter}")
+      cy.get('a#cartur.nav-link').contains('Cart').click()
+      cy.wait(1000).get('tr.success').each((product) => {
+        cy.wait(1000).wrap(product).within(() => {
         cy.contains('Delete').click()
         })
+      cy.get('tr.success').should('have.length',0)
       })
     })
-
-    //Check that the “Place order” button opens a modal window with the ordering process
+    
     it('“Place order” button opens a modal window with the ordering process', () => {
-      cy.get('.card').contains('Samsung galaxy s6').click()
-      cy.contains('Add to cart').click()
-      cy.contains('Cart').click()
-      cy.contains('Place Order').click()
+      cy.get('div.card').contains('Samsung galaxy s6').click()
+      cy.get('a.btn-success').contains('Add to cart').wait(1000).click().type("{enter}")
+      cy.get('a#cartur.nav-link').contains('Cart').click()
+      cy.get('button.btn-success').contains('Place Order').click()
       cy.get('.modal-dialog').should('be.visible')
     })
-
-    
 })
-*/
+
 
